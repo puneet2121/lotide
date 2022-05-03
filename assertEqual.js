5f01386699eacd90f5a8dbde8720ddce96fd6559
puneet2121 <bedipuneet21@gmail.com>
2022-05-03 02:59:02 +0000

    add code

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected){
+        console.log(`Assertion passed: ${actual} === ${expected}`)
+    } else {
+        console.log(`Assertion failed: ${actual} !== ${expected}`)
+    }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1,3)
