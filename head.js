eebd5ddba02decf9a6007a72e5ce380ff6ec87c3
Puneet Kumar <puneetkumar@Puneets-MacBook-Air.local>
2022-05-09 16:49:25 -0600

    fixing code

--- head.js
+++ head.js
@@ -1,5 +1,5 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected){
+    if (actual === expected) {
         console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`)
     } else {
         console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`)
@@ -12,5 +12,5 @@ console.log(head([1,3,4]));
 
 
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1,3)
+assertEqual(head([1,2,3]), 1);
+assertEqual(head([2,3,4]),3)
