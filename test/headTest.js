0877104fcbf14c663ae8c2573fbac42c4e985056
puneet2121 <bedipuneet21@gmail.com>
2022-05-10 01:35:03 +0000

    using mocha chai instead of own assertion

--- test/headTest.js
+++ test/headTest.js
@@ -1,20 +1,18 @@
-const assertEqual = require('../assertEqual');
-const head = function(arr) {
-  return arr[0];
-};
-console.log(head([1,3,4]));
+// const assertEqual = require('../assertEqual');
+// const head = function(arr) {
+//   return arr[0];
+// };
+// console.log(head([1,3,4]));
 
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(head([1,2,3]), 1);
-assertEqual(head([2,3,4]),3);
-const tail = function(arr) {
-  arr.shift();
-  return arr;
-};
-console.log(tail([1,3,4]));
-
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(head([1,2,3]), 1);
-assertEqual(head([2,3,4]),3);
+// assertEqual(head([1,2,3]), 1);
+// assertEqual(head([2,3,4]));
+const assert = require('chai').assert;
+const head = require('../head');
+describe("#head", () => {
+  it("returns 1 for [1,2,3]",() => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
+  it("returns '5' for ['5']",() => {
+    assert.strictEqual(head(['4']), '5');
+  });
+});
