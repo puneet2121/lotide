6f43e5c7efc9e7ffca3068c7a4c630fa3978a584
puneet2121 <bedipuneet21@gmail.com>
2022-05-10 01:46:49 +0000

    using chai and mocha and removing own assert function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,23 @@
-const assertEqual = require('../assertEqual');
-const tail = function(arr) {
-  const newarr = arr.slice(1,arr.length);
-  console.log(newarr);
-  
-};
+const tail = require('../tail');
+const assert = require('chai').assert;
+describe("#tail", () => {
+  it("returns the tail of the array all the values exculding the first",() => {
+    assert.deepEqual(tail([1,2,3,4],[2,3,4]));
+  });
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+
+
+
+
+
+// const tail = function(arr) {
+//   const newarr = arr.slice(1,arr.length);
+//   console.log(newarr);
+  
+// };
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
