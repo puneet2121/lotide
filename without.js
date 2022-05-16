8ff5cfeb648db8c13549505ca42fcaffa7300ce3
puneet2121 <bedipuneet21@gmail.com>
2022-05-16 02:34:11 +0000

    removing unwanted item

--- without.js
+++ without.js
@@ -1,16 +1,34 @@
+
+const eqArray = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2[i] !== arr1[i]) return false;
+  }
+  return true;
+};
+const assertArrayEqual = function(actual, expected) {
+  if (eqArray(actual,expected)) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
 const without = function(arr,itemsToRemove) {
-  for (i = 0; i < arr.length; i++) {
-    for(j= 0; j <itemsToRemove; j++){
-      if (arr[i] === itemsToRemove[j]){
-        arr.splice(i,itemsToRemove.length)
-        console.log(arr)
-    }
+  let newarr = [];
+  newarr = arr.slice(0);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < itemsToRemove; j++) {
+      if (arr[i] === itemsToRemove[j]) {
+        newarr.splice(i,itemsToRemove.length);
+      }
     
     }
     
   }
-  
-}
-arr = [1,2,2,3,8,8]
-itemsToRemove = [1]
-without(arr,itemsToRemove)
+  return newarr;
+};
+let arr = [1,2,2,3,8,8];
+let itemsToRemove = [8];
+console.log(without(arr,itemsToRemove));
+assertArrayEqual(arr,[1,2,2,3,8,8]);
