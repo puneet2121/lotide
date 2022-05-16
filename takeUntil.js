857c0c538feb51a63bf16a2bf50564136abfebe8
puneet2121 <bedipuneet21@gmail.com>
2022-05-16 16:24:53 +0000

    using require

--- takeUntil.js
+++ takeUntil.js
@@ -1,10 +1,4 @@
-const eqArray = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr2[i] !== arr1[i]) return false;
-  }
-  return true;
-};
+const eqArray = require('./eqArrays');
 const assertArrayEqual = function(actual, expected) {
   if (eqArray(actual,expected)) {
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
